81c4606d5ffac573fbeb3436a0b154179d389ba9
adarshnaikdev <nqvadi@gmail.com>
2024-12-31 13:10:01 +0530

    navigation header completed

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -6,7 +6,14 @@ function Navbar() {
     <div className='navbar'>
 
         <div className="navContainer">
-            
+            <span className="logo">
+              Booking.com
+            </span>
+
+            <div className="navItems">
+              <button className="navButton">Register</button>
+              <button className="navButton">Login</button>
+            </div>
         </div>
     </div>
   )
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./home.css";
 import Navbar from '../../Components/navbar/Navbar';
+import Header from '../../Components/header/Header';
 
 function Home() {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Navbar/>
+      <Header/>
+      </div>
   )
 }
 
